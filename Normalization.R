855c797db5b22813b72a47c75ad4f7921abbb9e6
KyleRodriguez96 <89027392+KyleRodriguez96@users.noreply.github.com>
2021-10-04 21:26:30 -0400

    Add files via upload
    Finalized version

--- Normalization.R
+++ Normalization.R
@@ -49,13 +49,51 @@ no_student_loans$NOLOAN_COMP_4YR_TRANS_YR8_RT <- as.numeric(no_student_loans$NOL
 completion_rate$C150_4.z <- (completion_rate$C150_4 - mean(completion_rate$C150_4,na.rm = T))/sd(completion_rate$C150_4, na.rm=TRUE)
 completion_rate$C150_L4.z <- (completion_rate$C150_L4 - mean(completion_rate$C150_L4,na.rm = T))/sd(completion_rate$C150_L4, na.rm=TRUE)
 
+student_loans$LOAN_COMP_ORIG_YR8_RT.z <- (student_loans$LOAN_COMP_ORIG_YR8_RT - mean(student_loans$LOAN_COMP_ORIG_YR8_RT,na.rm = T))/sd(student_loans$LOAN_COMP_ORIG_YR8_RT, na.rm=TRUE)
+student_loans$LOAN_COMP_4YR_TRANS_YR8_RT.z <- (student_loans$LOAN_COMP_ORIG_YR8_RT - mean(student_loans$LOAN_COMP_ORIG_YR8_RT,na.rm = T))/sd(student_loans$LOAN_COMP_ORIG_YR8_RT, na.rm=TRUE)
+student_loans$LOAN_COMP_2YR_TRANS_YR8_RT.z <- (student_loans$LOAN_COMP_2YR_TRANS_YR8_RT - mean(student_loans$LOAN_COMP_2YR_TRANS_YR8_RT,na.rm = T))/sd(student_loans$LOAN_COMP_2YR_TRANS_YR8_RT, na.rm=TRUE)
+
+no_student_loans$NOLOAN_COMP_ORIG_YR8_RT.z <- (no_student_loans$NOLOAN_COMP_ORIG_YR8_RT- mean(no_student_loans$NOLOAN_COMP_ORIG_YR8_RT,na.rm = T))/sd(no_student_loans$NOLOAN_COMP_ORIG_YR8_RT, na.rm=TRUE)
+no_student_loans$NOLOAN_COMP_4YR_TRANS_YR8_RT.z <- (no_student_loans$NOLOAN_COMP_4YR_TRANS_YR8_RT - mean(no_student_loans$NOLOAN_COMP_4YR_TRANS_YR8_RT,na.rm = T))/sd(no_student_loans$NOLOAN_COMP_4YR_TRANS_YR8_RT, na.rm=TRUE)
+no_student_loans$NOLOAN_COMP_2YR_TRANS_YR8_RT.z <- (no_student_loans$NOLOAN_COMP_2YR_TRANS_YR8_RT - mean(no_student_loans$NOLOAN_COMP_2YR_TRANS_YR8_RT,na.rm = T))/sd(no_student_loans$NOLOAN_COMP_2YR_TRANS_YR8_RT, na.rm=TRUE)
 
 #summary stats and z plots
+
+#completion
 par(mfrow=c(2,1))
 plot(completion_rate$C150_4.z, col='grey',pch=19)
 plot(completion_rate$C150_L4.z, col='grey',pch=19)
 fivenum(completion_rate$C150_4,na.rm = T)
-summary(completion_rate$C150_4)
 fivenum(completion_rate$C150_L4,na.rm = T)
+summary(completion_rate$C150_4)
 summary(completion_rate$C150_L4)
 
+#student loans
+par(mfrow=c(3,1))
+plot(student_loans$LOAN_COMP_ORIG_YR8_RT.z, ylim = range(-2,3), col='grey',pch=19)
+plot(student_loans$LOAN_COMP_4YR_TRANS_YR8_RT.z, ylim = range(-2,3), col='grey',pch=19)
+plot(student_loans$LOAN_COMP_2YR_TRANS_YR8_RT.z, ylim = range(-2,5), col='grey',pch=19)
+fivenum(student_loans$LOAN_COMP_ORIG_YR8_RT,na.rm = T)
+summary(student_loans$LOAN_COMP_ORIG_YR8_RT)
+fivenum(student_loans$LOAN_COMP_4YR_TRANS_YR8_RT,na.rm = T)
+summary(student_loans$LOAN_COMP_4YR_TRANS_YR8_RT)
+fivenum(student_loans$LOAN_COMP_2YR_TRANS_YR8_RT,na.rm = T)
+summary(student_loans$LOAN_COMP_2YR_TRANS_YR8_RT)
+
+#no student loans 
+par(mfrow=c(3,1))
+plot(no_student_loans$NOLOAN_COMP_ORIG_YR8_RT.z , ylim = range(-2,8), col='grey',pch=19)
+plot(no_student_loans$NOLOAN_COMP_2YR_TRANS_YR8_RT.z,ylim = range(-2,8), col='grey',pch=19)
+plot(no_student_loans$NOLOAN_COMP_4YR_TRANS_YR8_RT.z,ylim = range(-2,8), col='grey',pch=19)
+fivenum(no_student_loans$NOLOAN_COMP_ORIG_YR8_RT,na.rm = T)
+summary(no_student_loans$NOLOAN_COMP_ORIG_YR8_RT)
+fivenum(no_student_loans$NOLOAN_COMP_2YR_TRANS_YR8_RT,na.rm = T)
+summary(no_student_loans$NOLOAN_COMP_2YR_TRANS_YR8_RT)
+fivenum(no_student_loans$NOLOAN_COMP_4YR_TRANS_YR8_RT.z,na.rm = T)
+summary(no_student_loans$NOLOAN_COMP_4YR_TRANS_YR8_RT.z)
+
+
+#cutting data more
+completion_rate <- tail(completion_rate,-100000)
+student_loans <- tail(student_loans,-100000)
+no_student_loans <-tail(no_student_loans,-100000) 
